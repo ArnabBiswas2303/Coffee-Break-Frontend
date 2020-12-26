fbf56ab8deef5d9e4dff831419ea2ecc80918b1c
Tanya933 <tanya.rajpal0400@gmail.com>
2020-12-26 23:43:05 +0530

    2nd commit

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,6 +1,12 @@
+//import classes from "*.module.css";
 import React from "react";
-import "./AboutUs";
+import classes from "./AboutUs.module.css";
 
 export default function AboutUs() {
-  return <div>AboutUS</div>;
+  return (
+    <div className={classes.aboutUs}>
+      <div className={classes.aboutContent}>first</div>
+      <div className={classes.aboutImg}>second</div>
+    </div>
+  );
 }
