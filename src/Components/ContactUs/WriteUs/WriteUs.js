4acad904f75e1b55d5b24ff3b16dc940ef4156b5
Aaditya <aaditya7739008423@gmail.com>
2020-12-27 10:21:54 +0530

    Completed WriteUs part

--- src/Components/ContactUs/WriteUs/WriteUs.js
+++ src/Components/ContactUs/WriteUs/WriteUs.js
@@ -1,27 +1,91 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import classes from "./Writeus.module.css";
 import coffeebeans from "../../../assets/icons/coffeeBeans.svg";
 export default function WriteUs() {
+  const [details, setdetails] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [errors, seterrors] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const onchange = (e) => {
+    setdetails({ ...details, [e.target.name]: e.target.value });
+    seterrors({ ...errors, [e.target.name]: "" });
+  };
+
+  /*
+    Get the details as
+    name = details.name
+    email = details.email
+    message = details.message
+  */
+  const givedetails = (e) => {
+    e.preventDefault();
+    const err = {
+      name: "",
+      email: "",
+      message: "",
+    };
+    if (details.name === "") {
+      err.name = "*Please enter a valid name";
+      seterrors(err);
+    }
+    var pattern = new RegExp(
+      /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+    );
+    if (!pattern.test(details.email)) {
+      err.email = "*Please enter a valid email";
+      seterrors(err);
+    }
+    if (details.message === "") {
+      err.message = "*Please enter a valid message";
+    }
+  };
   return (
     <Fragment>
       <div className={classes["writeus"]}>
         <div className={classes["wu-heading"]}>
           <h1 className={classes["wu-h-text"]}>Write Us</h1>
         </div>
+
         <div className={classes["wu-form"]}>
+          <div className={classes["wu-error"]}>
+            <p>{errors.name}</p>
+          </div>
           <input
-            className={classes["wu-input"]}
-            placeholder="Your name"
+            name="name"
+            className={classes["wu-input-1"]}
+            placeholder="Your name *"
+            onChange={(e) => onchange(e)}
           ></input>
+          <div className={classes["wu-error"]}>
+            <p>{errors.email}</p>
+          </div>
           <input
-            className={classes["wu-input"]}
-            placeholder="Your email"
+            name="email"
+            className={classes["wu-input-2"]}
+            placeholder="Your email *"
+            onChange={(e) => onchange(e)}
           ></input>
-          <input
+          <div className={classes["wu-error"]}>
+            <p>{errors.message}</p>
+          </div>
+          <textarea
+            name="message"
             className={classes["wu-messege"]}
-            placeholder="Your message"
-          ></input>
-          <button className={classes["wu-submit"]}>Send</button>
+            placeholder="Your message *"
+            onChange={(e) => onchange(e)}
+          ></textarea>
+          <button
+            onClick={(e) => givedetails(e)}
+            className={classes["wu-submit"]}
+          >
+            Send
+          </button>
         </div>
         <div
           className={classes["wu-bean"]}
