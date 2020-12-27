0a12b7d54d1b599169fe9da3c6cfd01d8bcdf6b4
Aaditya <aaditya7739008423@gmail.com>
2020-12-27 21:00:09 +0530

    Completed the contact us component

--- src/Components/ContactUs/Locations/Location.js
+++ src/Components/ContactUs/Locations/Location.js
@@ -1,6 +1,11 @@
 import React, { Fragment, useState, useEffect } from "react";
 import classes from "./Location.module.css";
 import MapboxGLMap from "../Map/Map"; //Import the map component
+//Import the icons
+import Address from "../../../assets/icons/mapMarker.svg";
+import Mail from "../../../assets/icons/mail.svg";
+import Phone from "../../../assets/icons/phone.svg";
+
 export default function Location() {
   return (
     <Fragment>
@@ -10,14 +15,19 @@ export default function Location() {
         </div>
         <div className={classes["l-div"]}>
           <div className={classes["l-info"]}>
+            <img className={classes["l-icon"]} alt="Address" src={Address} />
             <h2 className={classes["l-h"]}>Address</h2>
             <div className={classes["l-para"]}>
               Coffee Break | Majhitar, Rangpo 737132, India
             </div>
+            <img className={classes["l-icon"]} alt="Phone" src={Phone} />
+
             <h2 className={classes["l-h"]}>Phone</h2>
             <div className={classes["l-para"]}>
               Tel: +91 123456789 Fax: +91 123123123
             </div>
+            <img className={classes["l-icon"]} alt="Mail" src={Mail} />
+
             <h2 className={classes["l-h"]}>Email</h2>
             <div className={classes["l-para"]}>coffeebreak@gmail.com</div>
           </div>
