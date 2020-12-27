069296157296982b418f696fcbbe99d01a1469e6
Aaditya <aaditya7739008423@gmail.com>
2020-12-27 23:10:34 +0530

    Add Contact Us component

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+if (typeof window.URL.createObjectURL === "undefined") {
+  window.URL.createObjectURL = () => {
+    // Do nothing
+    // Mock this function for mapbox-gl to work
+  };
+}
